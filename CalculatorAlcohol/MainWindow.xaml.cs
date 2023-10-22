63dffd4fc703e429f28b8e66c7f968b41a40e58b
L1lChicha <kalidailya1205@gmail.com>
2023-10-22 23:30:23 +0300

    Added data processing.

--- CalculatorAlcohol/MainWindow.xaml.cs
+++ CalculatorAlcohol/MainWindow.xaml.cs
@@ -25,11 +25,126 @@ namespace CalculatorAlcohol
         {
             InitializeComponent();
             quantityComboBox.IsDropDownOpen = false;
+
         }
 
-        private void typeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
+            string containerType = typeComboBox.Text;
+            bool containsDigits = containerType.Any(char.IsDigit);
 
+
+
+            //обработка данных поля с типом сосуда
+            if (containsDigits)
+            {
+                //если поле содержит неверный формат данных
+                MessageBox.Show("Field Glass type cannot contain numbers! Put correct data.");
+                typeComboBox.Text = "";
+                return;
+            }
+
+            //обработка данных поля с объемом сосуда
+            string containerCapacityText = volumeTextField.Text;
+            int containerCapacity;
+            if (string.IsNullOrEmpty(containerCapacityText))
+            {
+                containerCapacity = 0;
+                //пустая строка
+            }
+            else
+            {
+                if (int.TryParse(volumeTextField.Text, out containerCapacity))
+                {
+                    //all good
+                }
+                else
+                {
+                    //если поле содержит неверный формат данных
+                    MessageBox.Show("Field Volume cannot contain letters! Put correct data.");
+                    volumeTextField.Text = "";
+                    return;
+                }
+            }
+
+            //обработка данных поля с количеством сосудов
+            string numberOfContainersText = quantityComboBox.Text;
+            int numberOfcontainers;
+            if (string.IsNullOrEmpty(numberOfContainersText))
+            {
+                //делаю проверку на пустую строку
+                MessageBox.Show("Field Quantity cannot be empty! Put data in it.");
+                return;
+             
+            }
+            else
+            {
+                if (int.TryParse(quantityComboBox.Text, out numberOfcontainers))
+                {
+
+                }
+                else
+                {
+                    //если поле содержит неверный формат данных
+                    MessageBox.Show("Field Quantity cannot contain letters! Put correct data.");
+                    quantityComboBox.Text = "";
+                    return;
+                }
+            }
+
+            //обработка данных поля с процентным содержанием спирта
+            string alcoholProcentText = percentTextBox.Text;
+            int alcoholProcent;
+            if (string.IsNullOrEmpty(alcoholProcentText))
+            {
+                //делаю проверку на пустую строку
+                MessageBox.Show("Field % cannot be empty! Put data in it.");
+                return;
+               
+            }
+            else { 
+                if (int.TryParse(percentTextBox.Text, out alcoholProcent))
+                {
+
+                }
+                else
+                {
+                    //если поле содержит неверный формат данных
+                    MessageBox.Show("Field % cannot contain letters! Put correct data.");
+                    percentTextBox.Text = "";
+                    return;
+                }
+            }
+            //Проверка на случай если поля с типом и объемом пустые
+            if(typeComboBox.Text == "" && volumeTextField.Text == "")
+            {
+                MessageBox.Show("Fields Glass type and Volume in ml cannot be empty together!\n Put data in one of them.");
+                return;
+            }
+
+
+            //заполнение конструкторов в зависимости от данных, веденных пользователем 
+            if (containerCapacity == 0)
+            {
+                Calculating calculatingWithoutCapacity = new Calculating(containerType, numberOfcontainers, alcoholProcent);
+
+            }
+            else if (containerType == "")
+            {
+                Calculating calculatingWithoutType = new Calculating(containerCapacity, numberOfcontainers, alcoholProcent);
+            }   
+            else
+            {
+                Calculating calculatingFullInfo = new Calculating(containerType, containerCapacity, numberOfcontainers, alcoholProcent);
+            }
+           
         }
+
+
+       
     }
 }
